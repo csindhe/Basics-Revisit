6269390b30011a59d2c9ead5ece2eecb5941f09c
Chetan Sindhe <csindhe@gmail.com>
2018-06-04 22:42:20 +0530

    Adv

--- Advanced JS Topics/bkp/main_bkp.js
+++ Advanced JS Topics/bkp/main_bkp.js
@@ -5,6 +5,8 @@ let Todo = (() => {
         myMenu = document.querySelector(".menu");
         oppMenu = document.querySelector(".menu-icon");
         myEventListners();
+        //Read all collapsed positions.
+        collectProperties_(this.collapsedPositions_);
     }
 
     function myEventListners(){
@@ -26,11 +28,38 @@ let Todo = (() => {
         myMenu.classList.remove("menu--animatable");
     }
 
+    getBoundingClientRect = {
+        top: 178,
+        left: 201,
+        width: 234,
+        height: 240,
+        bottom: 418,
+        right: 435
+    }
+
     return {
-        init: init
+        init: init,
+        collectProperties_: function(target) {
+            var part, parts = Object.keys(this.elements_);
+            for(var p = 0; p < parts.length; p++){
+                part = parts[p];
+                target[part] = this.elements_[part].getBoundingClientRect();
+                target[part].opacity = parseFloat(window.getComputedStyle(
+                    this.elements_[part]).opacity);
+            }
+        }
     }
 
 })();
+
+var dataSortWorker = new Worker('sort-worker.js');
+dataSortWorker.postMessage(datatoSort);
+
+dataSortWorker.addEventListener('message', (evt) => {
+    var sortedData = evt.data;
+    //update on the screen.....
+});
+
 
 document.addEventListener("DOMContentLoaded", Todo.init);
 
